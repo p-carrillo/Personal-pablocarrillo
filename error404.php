f2d3cf4dfa75c10064ba2c09fd0e3038d642e2d6
Krleza <pablo.carrillo.garcia@gmail.com>
2015-04-14 11:42:49 +0200

    cambio en fotos

---
+++ error404.php
@@ -0,0 +1,157 @@
+<html lang="es">
+<head>
+   <title>Página de Pablo Carrillo, programador de aplicaciones informaticas</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="css/page_animations.css"/>
+    <link rel="stylesheet" type="text/css" href="css/normalize.css"/>
+    <link rel="stylesheet" type="text/css" href="css/animate.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,300italic|Open+Sans' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" href="img/favicon.png" />
+
+    <meta name="keywords" content="PHP, HTL5, CSS3, MySQL, bases de datos, programación web, desarrollo de plicaciones, SEO, SEM, google adowrods" />
+   <meta name="description" content="Página personal de Pablo Carrillo, programador de aplicaciones informaticas" />
+
+</head>
+<body class="theme_1">
+   <script>
+     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+     ga('create', 'UA-54668173-1', 'auto');
+     ga('require', 'displayfeatures');
+     ga('send', 'pageview');
+
+   </script>
+   <!-- Wrapper -->
+   <div id="wrapper">
+      <div class="container">
+         <!-- Row -->
+         <div class="row">
+
+            <!-- Main Menu -->
+            <div class="main_menu col-md-1">
+               <div class="responsive_menu_btn"><span class="fa fa-bars"></span></div>
+               <div class="row">
+                  <ul>
+                     <li>
+                        <a href="http://pablocarrillo.net/#home" class="active home"><i class="fa fa-home"><span>home</span></i></a>
+                     </li>
+                     <li>
+                        <a href="http://pablocarrillo.net/#about" class="about" data-id="2"><i class="fa fa-user"><span>sobre mi</span></i></a>
+                     </li>
+                     <li>
+                        <a href="http://pablocarrillo.net/#resume" class="resume" data-id="3"><i class="fa fa-paperclip"><span>cv</span></i></a>
+                     </li>
+                     <li>
+                        <a href="http://pablocarrillo.net/#portfolio" class="portfolio" data-id="4"><i class="fa fa-code"><span>trabajos</span></i></a>
+                     </li>
+                     <li>
+                        <a href="http://pablocarrillo.net/#blog" class="blog" data-id="5"><i class="fa fa-wordpress"><span>blog</span></i></a>
+                     </li>
+                     <li>
+                        <a href="http://pablocarrillo.net/#contact" class="contact" data-id="6"><i class="fa fa-envelope"><span>contacta</span></i></a>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+            <!-- /Main Menu -->
+
+            <!-- Main Content -->
+            <div class="main_content col-md-10">
+                  
+                  
+
+                  <div class="col-md-12">
+                     <h1 class="title color"><span class="text">Error 404, la página no existe</span></h1>
+                       <div class="main_slider cycle-slideshow">
+                        <div class="overlay">
+                        </div>
+                         <img src="img/slider_3.jpg" alt="img" />
+                     </div>
+                  </div>
+          
+      
+
+      
+
+            </div>
+            <!-- /Main Content -->
+            
+            <!-- Social Buttons -->
+            <div class="social_icons col-md-1">
+               <div class="row">
+                  <ul>
+                     <li>
+                        <a href="https://twitter.com/ZaranKrleza" target="_blank" class="twitter"><i class="fa fa-twitter"></i></a>
+                     </li>
+                     <li>
+                        <a href="https://github.com/Krleza" target="_blank" class="github"><i class="fa fa-github"></i></a>
+                     </li>
+                     <li class="more_social_icons_li">
+                        <a href="#" class="plus more_social_icons_btn"><i class="fa fa-plus"></i></a>
+                        <ul class="more_social_icons">
+                           <li>
+                              <a href="http://es.linkedin.com/pub/pablo-carrillo-garcia/83/47b/198" target="_blank" class="linkedin"><i class="fa fa-linkedin"></i></a>
+                           </li>
+                           <li>
+                              <a href="http://stackoverflow.com/users/3169224/krleza" target="_blank" class="stack-overflow"><i class="fa fa-stack-overflow"></i></a>
+                           </li>
+                           <li>
+                              <a href="https://vimeo.com/user20037542" target="_blank" class="vimeo"><i class="fa fa-vimeo-square"></i></a>
+                           </li>
+                        </ul>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+            <!-- /Social Buttons -->
+
+            <div class="clear"></div>
+
+         </div>
+
+         <div class="clear"></div>
+      </div>
+
+      <div class="clear"></div>
+   </div>
+   <!-- /Wrapper -->
+
+   <!-- Footer -->
+   <div class="footer col-md-12">
+      2014 &copy; Pablo Carrillo García. 
+   </div>
+   <!-- /Footer -->
+
+   <!-- Loading Overlay -->
+   <div class="loading_overlay">
+                <div><img class="loading" src="img/loading.gif" alt="img" /></div>
+   </div>
+   <!-- /Loading Overlay -->
+
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/mixitup.min.js"></script>
+    <script type="text/javascript" src="js/jquery.cycle2.min.js"></script>
+    <script type="text/javascript" src="js/modernizr.custom.js"></script>
+    <script type="text/javascript" src="js/jquery.hoverdir.js"></script>
+    <script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+    <script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+    <script type="text/javascript" src="js/jquery.magnific_popup.js"></script>
+    <script type="text/javascript" src="js/tweetie.min.js"></script>
+    <script src="http://maps.google.com/maps/api/js?sensor=false&amp;libraries=geometry&amp;v=3.7"></script>
+    <script type="text/javascript" src="js/maplace.js"></script>
+    <script type="text/javascript" src="js/jquery.ba-hashchange.js"></script>
+    <script type="text/javascript" src="js/jquery.appear.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</body>
+</html>
